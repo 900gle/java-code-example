eb705e611e8f2d04816d0f9adaedac4ddd8be96b
doo <doohyun.lee@homeplus.co.kr>
2022-05-21 13:32:13 +0900

    first commit

---
+++ src/main/java/com/doo/javacodeexample/JavaCodeExampleApplication.java
@@ -0,0 +1,13 @@
+package com.doo.javacodeexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCodeExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doo/javacodeexample/JavaCodeExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doo.javacodeexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodeExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
